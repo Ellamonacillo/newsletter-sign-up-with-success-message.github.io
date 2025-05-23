d72015b321e0b8927030c77872b35819def5fd29
ellamonacillo <ellamonacillo@gmail.com>
2025-05-23 17:55:40 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Frontend Mentor - Newsletter sign-up form with success message solution
+
+This is a solution to the [Newsletter sign-up form with success message challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+Newsletter Sign-Up with Success Message
+
+### Screenshot
+
+![](images/screenshot.png)
+
+### Links
+
+- Solution URL: [Solution URL]()
+- Live Site URL: [Github Pages]()
+
+## My Process
+
+### Built with
+
+- HTML5
+- JavaScript
+- Bootstrap
+- Validatorjs
+
+## Author
+
+- Website - [Mariella Monacillo](https://mariellamonacillo.netlify.app)
+- Frontend Mentor - [@ellamonacillo](https://www.frontendmentor.io/profile/ellamonacillo)
---
+++ src/script.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('#form')
+const main = document.getElementById('main')
+const submission = document.getElementById('submission')
+const email_input = document.querySelector('#email')
+const email_error = document.querySelector('#email_error')
+
+
+function handleSubmit(e) {
+    e.preventDefault()
+
+    const field = e.target.querySelector("input")
+    const data = { [field.name]: field.value}
+
+    const rules = {
+        email: 'required|email'
+    }
+
+    const validation = new Validator(data, rules)
+
+    email_error.textContent = ''
+
+    if (validation.fails()) {
+        email_error.textContent = 'Valid email required'
+        email_input.classList.add('isInvalid')
+    }
+    else {
+        email_error.textContent = '';
+        main.classList.toggle('d-none')
+        submission.classList.toggle('d-none')
+    }
+}
+form.addEventListener('submit', handleSubmit)
+
+
+const dismiss = document.getElementById('dismiss')
+dismiss.addEventListener('click', () => {
+    main.classList.toggle('d-none')
+    submission.classList.toggle('d-none')
+
+    form.reset()
+    email_error.textContent = ''
+    email_input.classList.remove('isInvalid') 
+})
